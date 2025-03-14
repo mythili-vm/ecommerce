c851a875b9c13d4d8e955db3344e2e099e80233f
mythili-vm <mythilimuthusamy9705@gmail.com>
2025-03-14 23:06:56 +0530

    updated

--- ecommerce-react/src/config/Api.ts
+++ ecommerce-react/src/config/Api.ts
@@ -1,2 +1,9 @@
 import axios from 'axios';
 export const API_URL="http://localhost:8080"
+
+export const api=axios.create({
+    baseURL:API_URL,
+    headers:{
+        'Content-Type':'application/json',
+    },
+});
